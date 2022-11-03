7f8349bfb5bc4a52c2e442578f3c42e070379c9b
Eugene <evgenij.maevskij@gmail.com>
2022-11-03 20:05:51 +0200

    Edit script.js Created a function that declare the winner using conditional statements (if, else if, else) Left comment to make method playerSelector case-insensitive

--- script.js
+++ script.js
@@ -18,14 +18,37 @@ console.log("Computer's choice is " + computerInput);
 // player selection
 
 const playerInput = prompt("Rock, Paper or Scissors? Make your choice!");
-  if(playerInput === 'Rock' || playerInput === 'Paper' || playerInput === 'Scissors'){
+
+  if (playerInput === 'Rock' || playerInput === 'Paper' || playerInput === 'Scissors'){
     console.log('You have chosen ' + `${playerInput}`);    
   } else {
     console.log('You have not made a choice! Try again?');
   }
 
+// declare a winner
 
+const playRound = function(playerSelection, computerSelection){
+  if (playerInput === 'Rock' && computerInput === 'Paper'){
+    console.log('Paper bits rock. You lose!');
+  } else if (playerInput === 'Rock' && computerInput === 'Scissors'){
+    console.log('Rock bits scissors. You win!');
+  } else if (playerInput === 'Rock' && computerInput === 'Rock'){
+    console.log('You tie!');
+  } else if (playerInput === 'Paper' && computerInput === 'Rock'){
+    console.log('Paper bits rock. You win!');
+  } else if (playerInput === 'Paper' && computerInput === 'Scissors'){
+    console.log('Scissors bits paper. You lose!');
+  } else if (playerInput === 'Paper' && computerInput === 'Paper'){
+    console.log('You tie!');
+  } else if (playerInput === 'Scissors' && computerInput === 'Rock'){
+    console.log('Rock bits scissors. You lose!');
+  } else if (playerInput === 'Scissors' && computerInput === 'Paper'){
+    console.log('Scissors bits paper. You win!');
+  } else {
+    console.log('You tie!');
+  }
+};
 
-// const playRound = function(playerSelection, computerSelection){
+playRound();
 
-// }
+// Make your function’s playerSelection parameter case-insensitive
