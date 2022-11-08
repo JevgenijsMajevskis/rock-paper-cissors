a4a693ce7b62fea0f41f0636c80f82b01f56406f
Eugene <evgenij.maevskij@gmail.com>
2022-11-08 13:32:02 +0200

    Edit script.js Created a function to play one round and declare a winner To test a function playRound set playerSelection to rock

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ console.log('Hello');
 
 const choices = ['rock', 'paper', 'scissors'];
 
+// computer selection
+
 function getComputerChoice(){
   const computerInput = choices[Math.floor(Math.random() * choices.length)];
   return computerInput;
 }
+
+// declare a winner
 
 function declareWinner(playerSelection, computerSelection){
   if(playerSelection === computerSelection){
@@ -21,4 +25,22 @@ function declareWinner(playerSelection, computerSelection){
   }
 }
 
-getComputerChoice();
+// 1 round game
+
+function playRound(playerSelection, computerSelection){
+  const result = declareWinner(playerSelection, computerSelection);
+  if(result == "Tie"){
+    return "It's a Tie!";
+  } else if(result == "Player won"){
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else{
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+// 5 rounds game
+
